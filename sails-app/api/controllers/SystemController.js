4b07e83bea0ad0b452f495388711b3d5a59fe359
Jacky Zhao <jacky.zhao@bunchball.com>
2016-01-26 17:26:41 -0800

    /available endpoint is ready

---
+++ sails-app/api/controllers/SystemController.js
@@ -0,0 +1,13 @@
+/**
+ * SystemsController
+ *
+ * @description :: Server-side logic for managing systems
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    available: function (req, res) {
+      console.log('here!');
+      return res.send("OK");
+    }
+};
